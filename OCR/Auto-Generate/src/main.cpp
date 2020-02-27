b617c9d4c5d715d3da4af321b7cbc18f055984fb
jgbroz <george_ashcroft@live.com.au>
2020-02-27 11:41:16 +0000

    Comment Code

--- OCR/Auto-Generate/src/main.cpp
+++ OCR/Auto-Generate/src/main.cpp
@@ -3,14 +3,14 @@
 #include<opencv2/imgproc/imgproc.hpp>
 #include<opencv2/ml/ml.hpp>
 #include<iostream>
-#include<vector>
+#include<vector> //Inclusion of the necessary libraries
 
-const int min_area = 100;
+const int min_area = 200;
 const int image_width = 20;
-const int image_height = 30;
+const int image_height = 30; //Defined constants 
 
 using namespace cv;
-using namespace std;
+using namespace std; //Using namespace cv/std will be merged with Zalan's Hex later on
 
 int main(){
 	Mat imgRGB;
@@ -20,36 +20,30 @@ int main(){
 	Mat imgBWCopy;
 
 	Mat GroupInts;
-	Mat FlattenedFloats;
-
-
-	int Orientation = 1;
+	Mat FlattenedFloats;//Definition of the needed Matrices
 	
 	
 	vector<vector<Point>> ptContours; 
 	vector<Vec4i> v4iHierarchy; 
-	vector <String> fileList;
+	vector <String> fileList;//Definition of vectors. Could be moved out of main. Just made them as I went.
 
-	string characterArray("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-	string orientation("0123");
-	string Directory("/home/jgbroz/Documents/Project-Hex/OCR/Auto-Generate/A/0/*.bmp");
+	string characterArray("ABCDEFGHIJKLMNOPQRSTUVWXYZ");//The alphabet needed to assign values to read in images
+	string orientation("0123");//Based on my peers file storage I had to accomadate for their file names.
+	string Directory("/home/jgbroz/Documents/Project-Hex/OCR/Auto-Generate/A/0/*.bmp");//The directory of the saved images. MUST BE ALTERED IF RAN ON A DIFFERENT SYSTEM.
 
-	for (int i = 0; i <= 25; i++){
-		Directory.replace(53, 1, characterArray, i, 1);
-		cout<<Directory<<"\n";
-		for (int j = 0; j <= 3; j++){
-			Directory.replace(55, 1, orientation, j, 1); 
-			glob(Directory, fileList, false);
-			for (size_t k = 0; k < fileList.size(); ++k){
-				cout<<"Displaying the file list:" <<fileList[k]<<"\n";
-				imgRGB = imread(fileList[k]);
-				imshow("Test Result", imgRGB);
-				cout<< characterArray[i];
+	for (int i = 0; i <= 25; i++){//For i less than twenty0five )size of alphabet)
+		Directory.replace(53, 1, characterArray, i, 1);//Replace value fifty-three in the directory with a character corresponding to i from the alphabet written above.
+		cout<<Directory<<"\n";//Prints out the current directory to terminal
+		for (int j = 0; j <= 3; j++){//Cycles through each of the orientations (zero-three)
+			Directory.replace(55, 1, orientation, j, 1); //Changes the directory further based on orientation 
+			glob(Directory, fileList, false);//Reads in a list of all the files in the directory and stores it in an array
+			for (size_t k = 0; k < fileList.size(); ++k){//Runs a for loop equal to the amount of files in the directory
+				imgRGB = imread(fileList[k]);//Reads in the first image
 
 				if (imgRGB.empty()){
 					cout<<"Image not in directory\n";
 					return(0);
-				}
+				}//Returns error code if the image isn't found.
 
 				cvtColor(imgRGB,imgGray,COLOR_RGB2GRAY);
 
@@ -75,58 +69,55 @@ int main(){
 						ptContours,
 						v4iHierarchy,
 						RETR_EXTERNAL,
-						CHAIN_APPROX_SIMPLE);
+						CHAIN_APPROX_SIMPLE);//Applies filters to the image to make it more readable.
 
-				for (int i = 0; i < ptContours.size(); i++){
-					if (contourArea(ptContours[i]) > min_area){
-						Rect rectangleChar = boundingRect(ptContours[i]);
-						rectangle(imgRGB, rectangleChar, Scalar(255,0,0),2);
+				for (int l = 0; l < ptContours.size(); l++){//For the number of contour sets read in repeat the process. Ideally should be one but left room for multiple characters found.
+					if (contourArea(ptContours[l]) > min_area){//Check if the contour exceeds the defined minimun region.
+						Rect rectangleChar = boundingRect(ptContours[l]);//Place a rectangle on the contours end points.
+						rectangle(imgRGB, rectangleChar, Scalar(255,0,0),2);//Draw the rectangle over the image RGB not necessary as user feedback is no longer limited
+						//Add an imshow for RGB and a waitKey(0) to see how the images are being interpretted. 
+
 						Mat matROI = imgBW(rectangleChar);
-						Mat matROIResized;
-						resize(matROI, matROIResized, Size(image_width, image_height));
+						Mat matROIResized;//Create a region of interest based on the highlighted region
+						resize(matROI, matROIResized, Size(image_width, image_height));//Resize the ROI based on the constants to be uniformly stored.
 
-						imshow("matROI",matROI);
-						imshow("matROIResized", matROIResized);
-						imshow("imgRGB", imgRGB);
 						
-						waitKey(0);
+						imshow("matROIResized", matROIResized);//Shows the final result to be stored. Again add a waitKey(0) at the end to really see it.
 						
-						cout<<"\n"<<v4iHierarchy[i]<<"\n";							
-						cout<<"\n"<<ptContours[i]<<"\n";
-						cout<<"valid input appending the xml files.\n";
-
-						GroupInts.push_back(int(characterArray[i]));
+						cout<<"valid input appending the xml files."<<characterArray[i]<<"\n";//Display what character is currently being appended to the xml file
+						
+						GroupInts.push_back(int(characterArray[i]));//Push the current value into a vector array 
 
 						Mat matImageFloat;
 
-						matROIResized.convertTo(matImageFloat, CV_32FC1);
+						matROIResized.convertTo(matImageFloat, CV_32FC1);//Convert the Resized image to float values using OpenCV 32FC1. Needed for the KNN inputs
 
-						Mat FlatFloats = matImageFloat.reshape(1,1);
+						Mat FlatFloats = matImageFloat.reshape(1,1);//Must be converted further to flat floats
 
-						FlattenedFloats.push_back(FlatFloats);
+						FlattenedFloats.push_back(FlatFloats);//Append the vector array with the new values of the xml file 
 						}
 					}
 				}
 			}	
 		}
 	
-	cout<<"Training Completed\n";
+	cout<<"Training Completed\n";//State the training is complete
 
-	FileStorage fsClassifications("group_ints.xml", FileStorage::WRITE);
+	FileStorage fsClassifications("group_ints.xml", FileStorage::WRITE);//Writes the new values to an xml file for the corresponding characters
 	if (fsClassifications.isOpened() == false){
 		cout<<"Unable to open group_ints\n";
 		return(0);
 	}
 
 	fsClassifications<<"groups"<<GroupInts;
-	fsClassifications.release();
+	fsClassifications.release();//Writes them in and releases the files
 
-	FileStorage fsTrainingImage("image.xml",FileStorage::WRITE);
+	FileStorage fsTrainingImage("image.xml",FileStorage::WRITE);//Same process
 	if (fsTrainingImage.isOpened() == false){
 		cout<<"Unable to open image\n";
 		return(0);
 	}
-	fsTrainingImage<<"images"<<FlattenedFloats;
+	fsTrainingImage<<"images"<<FlattenedFloats;//Write and release the xml file broken down to a pixel by pixel basis with an area of about 600. From my memory may be incorrect area.
 	fsTrainingImage.release();
 	return(0);
 }
--- OCR/Read the Result/src/main.cpp
+++ OCR/Read the Result/src/main.cpp
@@ -4,16 +4,17 @@
 #include<opencv2/ml/ml.hpp>
 
 #include<iostream>
-#include<sstream>
+#include<sstream> 
+//The necessary libraries to be used throughout the project
 
-const int minArea = 100;
+const int minArea = 200; //Defines the minimun area needed for a detected contour to be read as valid data.
 
 const int imageWidth = 20;
-const int imageHeight = 30;
+const int imageHeight = 30; //Values to be used to resize a region of interest
 
 using namespace std;
 
-using namespace cv;
+using namespace cv; //Pleb names spaces Not using HEX namespace yet needs to be combined
 
 
 class ContourData{
@@ -28,60 +29,56 @@ class ContourData{
 			       	return false;
 			}
 			return true;
-		}
+		}//Checks the validity of the read contour by counting the region area.
 	static bool sortContours(const ContourData& cwdLeft, const ContourData& cwdRight){
-		return(cwdLeft.boundingRect.x < cwdRight.boundingRect.x);
+		return(cwdLeft.boundingRect.x < cwdRight.boundingRect.x); //Sorts all read contours by there respective positions. Remnants of a dead code as 
+		//A proper filter method hasn't been applied yet.
 	}
-};
+};//Class to store necessary contour data to be compared with the XML files later on.
 
 int main(){
 	vector<ContourData> allData;
-	vector<ContourData> validData;
+	vector<ContourData> validData;//Declares to variables that fall under the class
 	cout<<"Start of the program\n";
-	Mat classification;
+	Mat classification;//Defines a matrix 
 	FileStorage fsClassifications("group_ints.xml", FileStorage::READ);
-
+	//Reads in the data from group_ints
 	if (fsClassifications.isOpened() == false){
 		cout<<"Error, can't locate groups_ints.xml\n";
 		return (0);
-	}
+	}//If empty display error message and exit
 
 	fsClassifications["groups"] >> classification;
-	fsClassifications.release();
-
-	cout <<"The matrix of classifications is" <<classification<<"\n";
+	fsClassifications.release();//Stores the values of group_ints.xml into classification
 
 	Mat trainingImage;
-	FileStorage fsImages("image.xml", FileStorage::READ);
+	FileStorage fsImages("image.xml", FileStorage::READ);//Reads in image.xml
 
 	if (fsImages.isOpened() == false){
 		cout<<"Error, can't locate image.xml\n";
-		return (0);
+		return (0);//Same error message Ahmed :/
 	}
 
-	fsImages["images"] >> trainingImage;
+	fsImages["images"] >> trainingImage;//Stores the values into the matrix trainingImage
 	fsImages.release();
 
-	cv::Ptr<cv::ml::KNearest> kNearest(cv::ml::KNearest::create());
+	cv::Ptr<cv::ml::KNearest> kNearest(cv::ml::KNearest::create());//Establishes the function to train a KNN model
 
-	kNearest->cv::ml::StatModel::train(trainingImage, cv::ml::ROW_SAMPLE, classification);
-
-	//Note at this point the training and generated data is complete. We would now upload the filtered image and test it. However for the sake of learning I tried some of my own img processing based off
-	//the last example
+	kNearest->cv::ml::StatModel::train(trainingImage, cv::ml::ROW_SAMPLE, classification); //Trains a model based on the values of classification & trainingImage.
 	
-	Mat matTestImage = imread("test_image.bmp");
+	Mat matTestImage = imread("test_image.bmp"); //Reads in a test_image
 
 	if (matTestImage.empty()){
 		cout<<"Error, test_image.png not found";
-		return (0);
+		return (0);//If empty return error
 	
 	}
 	Mat ThreshCopy;
 	Mat Blurred;	
 	Mat Grayscale;
-	Mat Thresh;
+	Mat Thresh;//Establishes more bloody matrices.
 	
-	cvtColor(matTestImage, Grayscale, COLOR_RGB2GRAY);
+	cvtColor(matTestImage, Grayscale, COLOR_RGB2GRAY);//Converts the read image to grey scale
 	
 	GaussianBlur(Grayscale,
         	Blurred,
@@ -95,19 +92,19 @@ int main(){
         	ADAPTIVE_THRESH_GAUSSIAN_C,       
         	THRESH_BINARY_INV,               
 	 	11,
-		2);
+		2);//Applies filters of the original image and then produces a new one. Ditto for the next filter.
 
-	ThreshCopy = Thresh.clone();
+	ThreshCopy = Thresh.clone();//Creates a copy of the image as findcontours overwrites the original image.
 	
 	
 	vector<vector<Point>> ptContours;
-	vector<Vec4i> v4iHierarchy;
+	vector<Vec4i> v4iHierarchy;//Creates vectors to store information from the thresh copy see below.
 	
 	findContours(ThreshCopy,
 			ptContours,
 			v4iHierarchy,
 			RETR_EXTERNAL,
-			CHAIN_APPROX_SIMPLE);
+			CHAIN_APPROX_SIMPLE);//Stores the location of the contours as a simplified four end point region and stores the needed data in the hierarchy and ptContours
 	
 	
 	for (int i = 0; i < ptContours.size(); i++){
@@ -115,41 +112,41 @@ int main(){
 		contourData.ptContour = ptContours[i];
 		contourData.boundingRect = boundingRect(contourData.ptContour);
 		contourData.area = contourArea(contourData.ptContour);
-		allData.push_back(contourData);
+		allData.push_back(contourData);//For each contour check of it's size is valid and then create a bounded rectangle around the region. 
 	}
 
 	for (int i=0; i < allData.size(); i++){
 		if (allData[i].validContour()){
-			validData.push_back(allData[i]);
+			validData.push_back(allData[i]);//Checks if each pushed back value was valid corresponding to the class' function and if it works push it back into the the validData area.
 		}
 	}
 
-	sort(validData.begin(), validData.end(), ContourData::sortContours);
+	sort(validData.begin(), validData.end(), ContourData::sortContours);//Sorts out the contours.
 
-	string ReadCharacters;
+	string ReadCharacters;//A string to store the read in results
 
-	for (int i = 0; i < validData.size(); i++){
+	for (int i = 0; i < validData.size(); i++){//For each vaild data value
 		rectangle(matTestImage,
 				validData[i].boundingRect,
 				Scalar(0,255,0),
 				2);
-		Mat matROI = ThreshCopy(validData[i].boundingRect);
+		Mat matROI = Thresh(validData[i].boundingRect);
 		Mat ROIResized;
-		resize(matROI, ROIResized, Size(imageWidth, imageHeight));
+		resize(matROI, ROIResized, Size(imageWidth, imageHeight));//Create a region of interest around the highlighted contours
 
 		Mat ROIFloat;
-		ROIResized.convertTo(ROIFloat, CV_32FC1);
-		
+		ROIResized.convertTo(ROIFloat, CV_32FC1);//Convert the ROI to a float
+		imshow("MatRoiResized",ROIResized);
 		Mat matCurrentChar(0,0, CV_32F);
 
-		kNearest->findNearest(ROIFloat.reshape(1,1), 5, matCurrentChar);
-		
-		float fltCurrentChar  = (float)matCurrentChar.at<float>(0,0);
+		kNearest->findNearest(ROIFloat.reshape(1,1), 3, matCurrentChar);//Use the KNN to try and compare the read in image to the gene
+			
+		float fltCurrentChar  = (float)matCurrentChar.at<float>(0,0);//Stores the result as a char
 
-		ReadCharacters = ReadCharacters + char(int(fltCurrentChar));
+		ReadCharacters = ReadCharacters + char(int(fltCurrentChar));//Appends the array with the resut
 	}
-	cout <<"\n"<<ReadCharacters<<"\n";
-	imshow("Initial_image", matTestImage);
+	cout <<"\n"<<ReadCharacters<<"\n";//Prints out the result
+	imshow("Initial_image", matTestImage);//Displays the image
 
 	waitKey(0);
 	return(0);
